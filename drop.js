f04b99fd87cb4c2722451030114ba0e9d55fa08a
gunshybadguy <loritchie@gmail.com>
2016-05-15 22:18:07 +0100

    made water rise

--- drop.js
+++ drop.js
@@ -1,8 +1,8 @@
 function Drop(){
   
   // random starting position
-  this.x = random(600);
-  this.y = random(300);
+  this.x = mouseX; 
+  this.y = mouseY;
 
 
   this.show = function(){
@@ -12,7 +12,7 @@ function Drop(){
     var my = random(this.y,this.y+14)
     
     stroke(50,random(100,222),222);
-    strokeWeight(2);
+    strokeWeight(10);
     line(this.x, this.y, mx,my);
     
     this.x = mx;
--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var drops = [];
 
 function setup() {
   createCanvas(600,300);
-  frameRate(60);
+  frameRate(10);
 
 } // setup
 
@@ -12,13 +12,11 @@ function setup() {
 function draw() {
   background(51);
 
-// make a drop and add to array 
-// 1 per frame 
-var drop = new Drop();
-drops.push(drop); 
+// make rain!
+//var drop = new Drop();
+//drops.push(drop); 
 
 
- 
 // show each drop
 for (var i =0; i<drops.length; i++){
     drops[i].show();
@@ -35,6 +33,22 @@ for (var i =0; i<drops.length; i++){
 
 }
 
+
+// fill up with water 
+for (var i =0; i<drops.length; i++){
+
+  if (drops[i].y >290){
+  fill(50,222,222); 
+  noStroke();
+  rect(0,295,600,5);
+  } // if
+} // for 
+
 } // draw 
 
+function mousePressed(){
+  console.log("pressed"); 
+  var dropp = new Drop();
+  drops.push(dropp); 
+}
 
